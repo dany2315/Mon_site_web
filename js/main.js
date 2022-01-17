4e1a608cfa28358370719e156f06f1a2835207ba
Dany serfaty <davidserfaty2315@gmail.com>
2022-01-18 01:21:51 +0200

    j'ai mis directement le 13eme boutton celui des voiture avec le formulaire je dois faire la fonction annule et le formulaire

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 //1er boutton
 function nombreDcimaux() {
     var num = parseFloat( prompt("ecrivait un chiffre,please!"));
+    while (isNaN(num)) {
+        num = parseFloat(prompt("Remplissez le champ correctement avec des chiffres"))
+    }
     var rond = Math.trunc(num);
     var decimal = num -rond;
     var message =`le chiffre rond est:${rond}\nle chiffre decimal est:${decimal}.`
@@ -10,8 +13,15 @@ function nombreDcimaux() {
 //2eme boutton
 function loyer() {
     
-    var salary = parseFloat( prompt("ecrivait votre salaire,please!"));
-    var rent = parseFloat( prompt("ecrivait votre loyer,please!"));
+    var salary = parseFloat(prompt("ecrivait votre salaire,please!"));
+    while (isNaN(salary)) {
+        salary = parseFloat(prompt("Remplissez le champ de votre salaire correctement avec des chiffres"))
+    }
+
+    var rent = parseFloat(prompt("ecrivait votre loyer,please!"));
+    while (isNaN(rent)) {
+        rent =parseFloat(prompt("Remplissez le champ de votre loyer correctement avec des chiffres"))
+    }
     var rentOwed = rent;
     var month = 1;
     var gift = false;
@@ -62,6 +72,22 @@ function formulairUser() {
             //{name: prompt("Quel est la carte"), number: parseInt(prompt("Numero de carte")), exp: prompt("Date d'expiration (mois/annee)"),cvv: parseInt(prompt("cvv")) },
         ],
     };
+    while (isNaN(user.id)) {
+        user.id = parseInt(prompt("Remplissez le champ de votre NUMERO D'IDENTITER correctement avec des chiffres"))
+    }
+    while (isNaN(user.ddn)) {
+        user.ddn = parseInt(prompt("Remplissez le champ de votre DATE DE NAISSANCE correctement avec des chiffres"))
+    }
+    while (isNaN(user.number)) {
+        user.number = parseInt(prompt("Remplissez le champ de votre NUMERO DE CARTE correctement avec des chiffres"))
+    }
+    while (isNaN(user.exp)) {
+        user.exp = parseFloat(prompt("Remplissez le champ DATE D'EXPIRATION correctement avec des chiffres"))
+    }
+    while (isNaN(user.cvv)) {
+        user.cvv = parseFloat(prompt("Remplissez le champ CVV correctement avec des chiffres"))
+    }
+
     var listCard= user.creditCards.join
     var utilisateur =`Prenom:${user.firstName.value} 
                     \nNom:${user.lastName.value}
@@ -86,14 +112,18 @@ function trouverName() {
         
     }
     var message=`Vous avez fait ${people} tantative de prenom avant de trouver DAVID
-                \n Et voivi la liste de prenom taper ${arr}`
+                \n Et voici la liste de prenom taper ${arr}`
     alert(message)
     
 }
 
 //5eme boutton
 function inversNum() {
-    var num =prompt("entrer un chiffre,SVP")
+
+    var num =prompt("entrer un nombre,SVP")
+    while (isNaN(num)) {
+        num =parseInt(prompt("Remplissez le champ correctement avec des chiffres"))
+    }
     var result= ""
     var dash= ""
     if (num<0) {
@@ -102,8 +132,114 @@ function inversNum() {
     }
     while (num!=0) {
         result+=num%10
-        num==Math.trunc(num/10)
+        num=Math.trunc(num/10) // g pa compris pk il faut mettre (/10)
     }
     result=result+dash
     alert(result)
 } 
+
+
+//13eme boutton
+
+function laCentral() {
+
+    var cars =[
+        {  
+            marqu :"FERRARI",
+            model :"F8",
+            cathegorie:["SPORT"],
+            price:"400 000 €",
+            image:"ferrari.jpg",
+        },
+        {   
+            marqu:"TESLA",
+            model:["model X"],
+            cathegorie:"FAMILLE",
+            price:"100 000 €",
+            image:"tesla.jpg",
+        },
+        {
+            marqu:"MERCEDES BENZ",
+            model:"viano",
+            cathegorie:["BUISSNES","FAMILLE"],
+            price:"70 000 €",
+            image:"mercedes_benz_viano.jpg",
+        },
+        {
+            marqu:"ROLL ROYCE",
+            model:"cullinan",
+            cathegorie:["LUXE"],
+            price:"500 000 €",
+            image:"roll_royce.jpg",
+        },
+        {
+            marqu:"JAGUAR",
+            model:"XJ50",
+            cathegorie:["LUXE"],
+            price:"80 000 €",
+            image:"jaguar.jpg",
+        },
+    ]
+    var body = document.querySelector("body")
+    var bontonApres=document.getElementById("a")
+
+
+
+    var divBarre = document.createElement("div")
+    divBarre.id="barre"
+    divBarre = body.insertBefore(divBarre,bontonApres);     //divBarre = je l'insere dans le body (divBarre je l'insere devant bouttonApres)
+    //b.appendChild(divBarre)
+
+    var divBox = document.createElement("div")
+    divBox.id="box"
+    divBox = body.insertBefore(divBox, divBarre);
+    //b.appendChild(divBox)
+
+    var divMain = document.createElement("div")
+    divMain.id="main"
+    divMain = body.insertBefore(divMain,divBox );      
+    //b.appendChild(divMain)
+
+    
+    
+
+    
+
+    var i =0
+    var toAppend =""
+
+    cars.forEach(createHTML)
+    divMain.innerHTML+=toAppend
+
+    function createHTML(car,i) {
+        toAppend+=`<div id="car${i+1}"  class="if ${car.cathegorie}" >
+                        <h3 class="cathegorie">${car.cathegorie}</h3>
+                        <div class="image"><img src="C:/Users/David/Desktop/webschool/projet-js/assets/image/${car.image}" alt="voiture" ></div>
+                        </br>
+                        <p class="marque">${car.marqu}</p>  
+                        </br>
+                        <p class="model">${car.model}</p>
+                        </br>
+                        <p class="price">${car.price}</p>
+                        </br>
+                        <button class="delete${car} close" onclick="annule(${i+1},cars)">x</button></div>`
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
