d548533000b91e80a87a9f76f4d6dd1bf5cf9c1b
Dany serfaty <davidserfaty2315@gmail.com>
2022-01-18 20:12:23 +0200

    en galere de faire marcher le x pour effacer la voiture

--- js/main.js
+++ js/main.js
@@ -200,11 +200,6 @@ function laCentral() {
     divMain = body.insertBefore(divMain,divBox );      
     //b.appendChild(divMain)
 
-    
-    
-
-    
-
     var i =0
     var toAppend =""
 
@@ -212,6 +207,39 @@ function laCentral() {
     divMain.innerHTML+=toAppend
 
     function createHTML(car,i) {
+        //var a ="car"
+        //var b = i+1
+        /*var divCar =document.createElement("div");
+        divCar.id=`car${i+1}`;
+        divCar.className=`if ${car.cathegorie}`
+
+        var h3 = document.createElement("h3");
+        h3.className=`cathegorie`;
+        h3.innerText=`${car.cathegorie}`
+
+        var divImage = document.createElement("div");
+        divImage.className="image";
+        divImage.innerText=`<img src="C:/Users/David/Desktop/webschool/projet-js/assets/image/${car.image}" alt="voiture" >`;
+
+        var espace = document.createElement("br");
+        
+        var marque = document.createElement("p");
+        marque.className="marque";
+        marque.innerText=`${car.marqu}`;
+
+        var espace2 = document.createElement("br");
+
+        var model = document.createElement("p");
+        model.className="model";
+        model.innerText=`${car.model}`;
+
+        var espace3 = document.createElement("br");
+
+        var*/
+
+
+
+
         toAppend+=`<div id="car${i+1}"  class="if ${car.cathegorie}" >
                         <h3 class="cathegorie">${car.cathegorie}</h3>
                         <div class="image"><img src="C:/Users/David/Desktop/webschool/projet-js/assets/image/${car.image}" alt="voiture" ></div>
@@ -222,12 +250,17 @@ function laCentral() {
                         </br>
                         <p class="price">${car.price}</p>
                         </br>
-                        <button class="delete${car} close" onclick="annule(${i+1},cars)">x</button></div>`
+                        <button class="delete${car} close" onclick="effacer("car",i+1)">x</button></div>`
+        
+    
+
         
     }
 
-
-
+    function effacer(a,b){
+        var div=document.getElementById(`${a+b}`)
+        div.remove()
+    }
 
 
 
